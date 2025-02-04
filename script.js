98c67fe397d0532c97781370b20515a977f939be
Darya <karpovich1310@gmail.com>
2025-02-04 21:21:09 +0300

    написала функции для добавления и сохранения введенных чисел

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 let outputScreen = document.getElementById("outputscreen");
 
+window.onload = function () {
+    let savedExpression = localStorage.getItem("calculatorExpression");
+    if (savedExpression) {
+        outputScreen.value = savedExpression;
+    }
+};
 
-function display(num)
+function display(value) {
+    outputScreen.value += value;
+    saveToLocalStorage();
+}
+
+function calculate() {
+    try {
+        let expression = outputScreen.value;
+        let result = evaluateExpression(expression);
+        outputScreen.value = result;
+        saveToLocalStorage();
+    } catch (error) {
+        alert("Ошибка в выражении");
+        outputScreen.value = "";
+    }
+}
+   
+function Clear()
 {
-    outputScreen.value += num
-}
-function Calculate(){
-    try
-    {
-        outputScreen.value = eval (outputScreen.value);
-    }
-    catch(err)
-    {
-        alert("Ошибочка")
-    }
-}    
-function Clear(){
     outputScreen.value = "";
+    localStorage.removeItem("calculatorExpression");
 }
-function del() {
-    outputScreen.value = outputScreen.value.slice(0,-1)
+function del() 
+{
+    outputScreen.value = outputScreen.value.slice(0, -1);
+    saveToLocalStorage();
 }
